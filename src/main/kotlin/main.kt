18a9dcb6e29d1cceae3d8be3c1a6745ec20216ef
Talita Oliveira <litaa.oliveira@gmail.com>
2021-03-31 21:32:10 -0300

    using class calculator on main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,8 @@
 fun main(args: Array<String>) {
     println("Hello World!")
+
+    val calculator = Calculator()
+    val double = calculator.double(5)
+
+    println("The double of 5 is $double")
 }
